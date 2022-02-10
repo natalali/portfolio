2b2b0c6b7cba6f717a32acfd2c1921afdb2a2cc1
bananu7 <bananu7@o2.pl>
2022-02-10 18:49:55 +0100

    Changed hamburger to only appear with js enabled

--- hamburger.js
+++ hamburger.js
@@ -1,3 +1,11 @@
+// Progressive enhancement - only create the hamburger if
+// scripts are enabled
+const hbg = document.createElement("div");
+hbg.classList.add('Hamburger');
+const hgbDiv = document.createElement("div");
+hbg.appendChild(hgbDiv);
+document.querySelector("header").prepend(hbg);
+
 document.querySelector(".Hamburger").addEventListener("click",
     () => {
         document.querySelector(".Menu").classList.toggle("hidden");
